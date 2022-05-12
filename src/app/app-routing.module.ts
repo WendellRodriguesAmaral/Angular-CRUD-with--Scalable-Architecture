0ec7c539a945ab9781124a79877bd37b8af709f6
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2022-05-12 14:26:58 -0300

    feat: tela de login pronta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: ''}
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'login', 
+    loadChildren: () => import('./pages/login/login.module')     // so carrega o modulo quando acesssar 'home'
+      .then(m => m.LoginModule)  // 'm' é o modulo recebido
+  },
 ];
 
 @NgModule({
