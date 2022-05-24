9c98accbf6a2b2db53cebf12e438e7a552e5f84e
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2022-05-24 11:52:49 -0300

    feat: habilita botão de ocultar e ver senha

--- src/app/pages/login/form-login/form-login.component.ts
+++ src/app/pages/login/form-login/form-login.component.ts
@@ -10,16 +10,22 @@ export class FormLoginComponent implements OnInit {
   email: string='';
   password: string='';
 
+  passwordView='password';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  login(){
+  login():void{
     console.log({email: this.email, password: this.password})
     
     //momentaneo
     window.location.reload();  //recarrega a pagina
+  }
+
+  changePasswordView():void {
+    this.passwordView == 'password' ? this.passwordView = 'text' : this.passwordView = 'password';
   }
 
 }
